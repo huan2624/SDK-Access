0cc997e7a64fc69d5b9f734bc5d9b1b481ed4ff5
mugua001 <262485836@qq.com>
2016-11-09 11:28:46 +0800

    init

---
+++ Unity_SDK/Assets/Scripts/SdkPanel.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class SdkPanel : MonoBehaviour {
+
+    //初始化sdk
+    public Button InitSdk;
+    //登录
+    public Button LoginBtn;
+    //登出
+    public Button LogoutBtn;
+    //退出游戏
+    public Button ExitBtn;
+    //获取角色信息
+    public Button InfoBtn;
+    //支付
+    public Button PayBtn;
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
